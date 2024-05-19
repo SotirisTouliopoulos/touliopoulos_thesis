95cd173358cc5ea654a6c8979ed85e6d12428c3e
SotirisTouliopoulos <s.v.touliopoulos@gmail.com>
2024-05-19 19:26:56 +0300

    fix for ENDMDL

--- filter_pdb_format.py
+++ filter_pdb_format.py
@@ -2,11 +2,8 @@
 import sys
 
 with open(sys.argv[1], 'r') as file:
-    print('MODEL        1')
     for line in file:
-        if line.startswith('ATOM') or line.startswith('HETATM'):
+        if line.startswith('ATOM') or line.startswith('HETATM') or line.startswith("MODEL"):
             #only A conformations for LYS , ARG etc
             if line[16] == 'A' or line[16] == ' ' :
                 print(line[:-1])
-        elif line.startswith('ENDMDL') :
-            break
--- mutate_pdb_format.py
+++ mutate_pdb_format.py
@@ -6,14 +6,13 @@ import sys
 
 try:    
     with open(sys.argv[1], 'r') as file:
-        print('MODEL        1')
         for line in file:
-            if line.startswith('ATOM'):
+            if line.startswith("MODEL"):
+                print(line[:-1])
+            
+            elif line.startswith('ATOM'):
                 if line[16] == 'A' or line[16] == ' ' :
                     print(line[:-1])
-                    #pass
-            elif line.startswith('ENDMDL') :
-                break
         
             elif line.startswith("HETATM"):
                 residue = line[17:20] 
